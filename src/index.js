3d5693fcd582d152b7c5ed12445410e590ddb883
Adrian Herrera <raiman00@hotmail.com>
2020-01-31 08:44:59 -0400

    Added publication's crud

--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const cors = require("cors");
 
 // initializations
 const app = express();
+app.use(cors());
 
 // settings
-app.set('port', process.env.PORT || 4000);
+app.set("port", process.env.PORT || 4000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
+
 
 // Global Variables
 app.use((req, res, next) => {
-
-    next();
-})
+  next();
+});
 
 // Routes
-app.use(require('./routes/'));
-app.use(require('./routes/authentication'));
-app.use('/links',require('./routes/links'));
+app.use(require("./routes/"));
+app.use(require("./routes/authentication"));
+app.use("/links", require("./routes/links"));
 
-// Public 
-app.use(express.static(path.join(__dirname, 'public')));
+// Public
+app.use(express.static(path.join(__dirname, "public")));
 
 // Starting the server
-app.listen(app.get('port'), () => {
-    console.log('Server on port', app.get('port'));
+app.listen(app.get("port"), () => {
+  console.log("Server on port", app.get("port"));
 });
--- src/routes/links.js
+++ src/routes/links.js
@@ -1,10 +1,60 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const pool = require('../database');
+const pool = require("../database");
 
-router.post('/add', (req, res) => {
-    res.send('Form');
+router.post("/newPost", async (req, res) => {
+  const { Title, Description,  Body, Id_cat, Public } = req.body.data;
+  const newLink = {
+    Title,
+    Description,
+    Body,
+    Id_cat,
+    Public
+  };
+  await pool.query("INSERT INTO articles set ?", [newLink]);
+  res.status(200).send("Recibido");
+});
+
+router.post("/editPost", async (req, res) => {
+  const { Id_art, Title, Description,  Body, Id_cat, Public } = req.body.data;
+  const editPost = {
+    Id_art,
+    Title,
+    Description,
+    Body,
+    Id_cat,
+    Public
+  };
+  await pool.query("UPDATE articles set ? WHERE Id_art = ?", [editPost, editPost.Id_art]);
+  console.log(editPost);
+  console.log(editPost.Id_art);
+  res.status(200).send("Recibido");
+});
+
+// get
+
+router.get("/postList", async (req, res) => {
+  const data = await pool.query("SELECT * FROM articles");
+  //console.log(data);
+  res.send(data);
+});
+
+router.get("/CategoryList", async (req, res) => {
+  const data = await pool.query("SELECT * FROM categories");
+  //console.log(data);
+  res.send(data);
+});
+
+router.get("/updatePost/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const data = await pool.query("SELECT * FROM articles WHERE Id_art = ?", [
+    id
+  ]);
+  // console.log(id);
+  res.send(data);
+  // res.send("Editado");
 });
 
 module.exports = router;
